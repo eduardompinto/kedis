03a599fd47e5cea2f5067f01a13c3667139a3768
Eduardo Martins Pinto <eduardo.90.martins@gmail.com>
2020-04-27 07:43:41 +0200

    Refactor

--- src/Parser.kt
+++ src/Parser.kt
@@ -1,12 +1,19 @@
+private const val SIMPLE_STRINGS = '+'
+private const val INTEGERS = ':'
+private const val ERRORS = '-'
+private const val BULK_STRINGS = '$'
+private const val ARRAYS = '*'
+private const val CRLF = "\r\n"
+
 class Command(
     private vararg val args: String
 ) : Iterable<String> {
 
     private fun chunks() = sequence {
-        yield("*${args.size}\r\n")
+        yield("${ARRAYS}${args.size}${CRLF}")
         args.forEach { arg ->
-            yield("\$${arg.length}\r\n")
-            yield("${arg}\r\n")
+            yield("${BULK_STRINGS}${arg.length}${CRLF}")
+            yield("${arg}${CRLF}")
         }
     }
 
@@ -21,15 +28,16 @@ data class Response(
     val success = errorCode == null
 }
 
-
 fun parse(msg: String): Response {
     val msgType = msg[0]
     val msgBody = msg.substring(1)
+
     return when (msgType) {
-        '+' -> Response(text = msgBody.trim())
-        ':' -> Response(integer = msgBody.toInt())
-        '-' -> Response(errorCode = msgBody.substring(0, 1), text = msgBody.substring(1))
-        '$' -> Response(text = msgBody.substring(1))
+        ARRAYS -> throw NotImplementedError("No implementation found for arrays msg type")
+        BULK_STRINGS -> Response(text = msgBody.substring(1))
+        ERRORS -> Response(errorCode = msgBody.substring(0, 1), text = msgBody.substring(1))
+        INTEGERS -> Response(integer = msgBody.toInt())
+        SIMPLE_STRINGS -> Response(text = msgBody.trim())
         else -> throw RuntimeException("Invalid Response [$msg]")
     }
 }
